95d1b79679907b0cbeefe2c6c8ca967e95378ae5
Yannick Heinrich <yannick.heinrich@gmail.com>
2016-09-15 14:12:11 +0200

    Initial Commit

---
+++ Palette/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Palette
+//
+//  Created by Yannick Heinrich on 15.09.16.
+//  Copyright © 2016 yageek. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
